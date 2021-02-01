24222c2fd136260f501192c810d04e60e549d0da
HansKenedy <kenedy.97_nd@hotmail.com>
2021-02-01 11:20:23 -0500

    Practica

--- src/app/components/book-list-example/book-list-example.component.ts
+++ src/app/components/book-list-example/book-list-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat } from 'rxjs';
 import { IBook } from '../book';
 import { BookData } from './mock-data';
 
@@ -11,11 +12,14 @@ export class BookListExampleComponent implements OnInit {
 
   books: IBook[];
   selectedBooks: IBook[];
+  count: number = 0;
+  listaBook: IBook[];
   constructor() { }
 
   ngOnInit(): void {
     this.books = this.fetchBook();
     this.selectedBooks = [];
+    this.listaBook = this.fetchBook();
   }
 
   fetchBook(): IBook[] {
@@ -24,6 +28,12 @@ export class BookListExampleComponent implements OnInit {
 
   selectBook(book: IBook): void {
     this.selectedBooks.push(book);
+    for(let lista in this.books){
+      if(this.books[lista] === book){       
+       this.books[lista].cantidad = this.books[lista].cantidad + 1;       
+      }
+    }
+
   }
 
 }
--- src/app/components/book-select-count/book-select-count.component.ts
+++ src/app/components/book-select-count/book-select-count.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { IBook } from '../book';
 
 @Component({
   selector: 'app-book-select-count',
@@ -8,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class BookSelectCountComponent implements OnInit {
 
   @Input() totalBooks: number;
+  @Input() cantidaSeleccionados: IBook;
   constructor() { }
 
   ngOnInit(): void {
